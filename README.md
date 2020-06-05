3fd49c2ba935d5a5d4f774ec9ce1f4f0cdc52c84
diallomouhamed <mouhamed.dia2l@yahoo.fr>
2020-06-05 18:52:32 +0200

    Màj README et crétion de la classe de test

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Ajouter votre fichier de donnée csv "df_matches.csv" dans le dossier "data"
 ## Lancement
 
 Pour lancer le projet, utiliser la commande "spark-submit --master local --py-files dist\FootballApp-0.1-py3.7.egg launch.py"
+
+## A propos
+
+Lors du lancement des tests, une erreur d'import de module survient. Ils n'ont donc pu être implémenté.
--- esgi/exo/FootballAppTest.py
+++ esgi/exo/FootballAppTest.py
@@ -1,6 +1,16 @@
 import random
 import unittest
+from esgi.exo.FootballApp import FootballApp
 
 class FootballAppTest(unittest.TestCase):
 
     """Tests de la classe FootballApp."""
+
+    def setUp(self):
+        """Initialisation des tests."""
+        self.footballApp = FootballApp()
+
+    def testCreateSparkSession(self):
+        """Test le fonctionnement de la fonction 'createSparkSession'."""
+        sparkSession = self.footballApp.createSparkSession()
+        self.assertIsNotNone(sparkSession)
